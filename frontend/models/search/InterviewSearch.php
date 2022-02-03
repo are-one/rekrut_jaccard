7ca891abae41c1d2529732f21ca903658bed4de7
Are1PM <arwanpriantomangidi@gmail.com>
2022-02-03 11:51:53 +0800

    [UPDATE] create search model frontend

---
+++ frontend/models/search/InterviewSearch.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace frontend\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Interview;
+
+/**
+ * InterviewSearch represents the model behind the search form of `frontend\models\Interview`.
+ */
+class InterviewSearch extends Interview
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['id', 'lowongan_id'], 'integer'],
+            [['pelamar_nik'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Interview::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'id' => $this->id,
+            'lowongan_id' => $this->lowongan_id,
+        ]);
+
+        $query->andFilterWhere(['like', 'pelamar_nik', $this->pelamar_nik]);
+
+        return $dataProvider;
+    }
+}
---
+++ frontend/models/search/PelamarSearch.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace frontend\models\search;
+
+use yii\base\Model;
+use yii\data\ActiveDataProvider;
+use frontend\models\Pelamar;
+
+/**
+ * PelamarSearch represents the model behind the search form of `frontend\models\Pelamar`.
+ */
+class PelamarSearch extends Pelamar
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['nik', 'nama_lengkap', 'tempat_lahir', 'tanggal_lahir', 'alamat', 'no_hp', 'email', 'file_cv', 'file_ijazah'], 'safe'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function scenarios()
+    {
+        // bypass scenarios() implementation in the parent class
+        return Model::scenarios();
+    }
+
+    /**
+     * Creates data provider instance with search query applied
+     *
+     * @param array $params
+     *
+     * @return ActiveDataProvider
+     */
+    public function search($params)
+    {
+        $query = Pelamar::find();
+
+        // add conditions that should always apply here
+
+        $dataProvider = new ActiveDataProvider([
+            'query' => $query,
+        ]);
+
+        $this->load($params);
+
+        if (!$this->validate()) {
+            // uncomment the following line if you do not want to return any records when validation fails
+            // $query->where('0=1');
+            return $dataProvider;
+        }
+
+        // grid filtering conditions
+        $query->andFilterWhere([
+            'tanggal_lahir' => $this->tanggal_lahir,
+        ]);
+
+        $query->andFilterWhere(['like', 'nik', $this->nik])
+            ->andFilterWhere(['like', 'nama_lengkap', $this->nama_lengkap])
+            ->andFilterWhere(['like', 'tempat_lahir', $this->tempat_lahir])
+            ->andFilterWhere(['like', 'alamat', $this->alamat])
+            ->andFilterWhere(['like', 'no_hp', $this->no_hp])
+            ->andFilterWhere(['like', 'email', $this->email])
+            ->andFilterWhere(['like', 'file_cv', $this->file_cv])
+            ->andFilterWhere(['like', 'file_ijazah', $this->file_ijazah]);
+
+        return $dataProvider;
+    }
+}

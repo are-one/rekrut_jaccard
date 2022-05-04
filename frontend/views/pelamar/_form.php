327488486e52999d0baa68ca9865f447fbd3cd7b
Are1PM <arwanpriantomangidi@gmail.com>
2022-05-04 20:23:22 +0800

    [UPDATE] Pelamar - Frontend/views

--- frontend/views/pelamar/_form.php
+++ frontend/views/pelamar/_form.php
@@ -1,5 +1,6 @@
 <?php
 
+use kartik\date\DatePicker;
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
@@ -12,23 +13,30 @@ use yii\widgets\ActiveForm;
 
     <?php $form = ActiveForm::begin(); ?>
 
-    <?= $form->field($model, 'nik')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nik')->textInput(['maxlength' => true, 'disabled' => true])->label('NIK') ?>
 
-    <?= $form->field($model, 'nama_lengkap')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'nama_lengkap')->textInput(['maxlength' => true])->label('Nama Lengkap') ?>
 
-    <?= $form->field($model, 'tempat_lahir')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'tempat_lahir')->textInput(['maxlength' => true])->label('Tempat Lahir') ?>
 
-    <?= $form->field($model, 'tanggal_lahir')->textInput() ?>
+    <?= $form->field($model, 'tanggal_lahir')->widget(DatePicker::classname(), [
+        'options' => ['placeholder' => 'Pilih Tanggal Lahir...'],
+        'pluginOptions' => [
+            'autoclose'=>true,
+            'format' => 'yyyy-mm-dd'
+        ]
+    ])->label('Tanggal Lahir') ; ?>
+
 
     <?= $form->field($model, 'alamat')->textarea(['rows' => 6]) ?>
 
-    <?= $form->field($model, 'no_hp')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'no_hp')->textInput(['maxlength' => true])->label('No. Handphone') ?>
 
     <?= $form->field($model, 'email')->textInput(['maxlength' => true]) ?>
 
-    <?= $form->field($model, 'file_cv')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'uploadCv')->fileInput(['maxlength' => true])->label('File CV') ?>
 
-    <?= $form->field($model, 'file_ijazah')->textInput(['maxlength' => true]) ?>
+    <?= $form->field($model, 'uploadIjazah')->fileInput(['maxlength' => true])->label("File Ijazah") ?>
 
     <div class="form-group">
         <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
--- frontend/views/pelamar/update.php
+++ frontend/views/pelamar/update.php
@@ -5,14 +5,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model frontend\models\Pelamar */
 
-$this->title = 'Update Pelamar: ' . $model->nik;
+$this->title = 'Edit Profile';
 $this->params['breadcrumbs'][] = ['label' => 'Pelamars', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->nik, 'url' => ['view', 'nik' => $model->nik]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="pelamar-update">
+<div class="pelamar-update shadow bg-white rounded p-3">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1 class="mb-3"><?= Html::encode($this->title) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,

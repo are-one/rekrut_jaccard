d09029510a8fe3d26605f823a0667211526139f8
Are1PM <arwanpriantomangidi@gmail.com>
2022-05-04 20:18:43 +0800

    [UPDATE] Hasil interview - Frontend/view

--- frontend/views/hasil-interview/index.php
+++ frontend/views/hasil-interview/index.php
@@ -9,36 +9,43 @@ use yii\grid\GridView;
 /* @var $searchModel frontend\models\search\HasilInterviewSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Hasil Interviews';
+$this->title = 'Hasil Interview';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="hasil-interview-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="white_shd full margin_bottom_30">
+    <div class="full graph_head">
+        <div class="heading1 margin_0">
+            <h2><small><?= Html::encode($this->title) ?></small></h2>
+        </div>
+    </div>
 
-    <p>
-        <?= Html::a('Create Hasil Interview', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    <div class="full price_table padding_infor_info">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive-sm">
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        // 'filterModel' => $searchModel,
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                            // 'id',
+                            'interview.lowongan.nama',
+                            'keterangan:ntext',
+                            [
+                                'class' => ActionColumn::className(),
+                                'urlCreator' => function ($action, \frontend\models\HasilInterview $model, $key, $index, $column) {
+                                    return Url::toRoute([$action, 'id' => $model->id, 'interview_id' => $model->interview_id]);
+                                }
+                            ],
+                        ],
+                    ]); ?>
 
-            'id',
-            'keterangan:ntext',
-            'interview_id',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, HasilInterview $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'id' => $model->id, 'interview_id' => $model->interview_id]);
-                 }
-            ],
-        ],
-    ]); ?>
-
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>

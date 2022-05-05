55cd29447b1809553e554c9ee7412cd29419adab
Are1PM <arwanpriantomangidi@gmail.com>
2022-05-05 12:49:07 +0800

    [UPDATE] Pelamar - backend/views

--- backend/views/pelamar/index.php
+++ backend/views/pelamar/index.php
@@ -9,42 +9,57 @@ use yii\grid\GridView;
 /* @var $searchModel backend\models\search\PelamarSearch */
 /* @var $dataProvider yii\data\ActiveDataProvider */
 
-$this->title = 'Pelamars';
+$this->title = 'Daftar Pelamar';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="pelamar-index">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+<div class="white_shd full margin_bottom_30">
+    <div class="full graph_head">
+        <div class="heading1 margin_0">
+            <h2><small><?= Html::encode($this->title) ?></small></h2>
+        </div>
+    </div>
 
-    <p>
+    <div class="full price_table padding_infor_info">
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive-sm">
+                    <!-- <p>
         <?= Html::a('Create Pelamar', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+    </p> -->
 
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
+                    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
+                    <?= GridView::widget([
+                        'dataProvider' => $dataProvider,
+                        'filterModel' => $searchModel,
+                        'headerRowOptions' => ['class' => 'table-bordered'],
+                        'columns' => [
+                            ['class' => 'yii\grid\SerialColumn'],
 
-            'nik',
-            'nama_lengkap',
-            'tempat_lahir',
-            'tanggal_lahir',
-            'alamat:ntext',
-            //'no_hp',
-            //'email:email',
-            //'file_cv',
-            //'file_ijazah',
-            [
-                'class' => ActionColumn::className(),
-                'urlCreator' => function ($action, Pelamar $model, $key, $index, $column) {
-                    return Url::toRoute([$action, 'nik' => $model->nik]);
-                 }
-            ],
-        ],
-    ]); ?>
+                            'nik',
+                            'nama_lengkap',
+                            'tempat_lahir',
+                            'tanggal_lahir',
+                            'alamat:ntext',
+                            //'no_hp',
+                            //'email:email',
+                            //'file_cv',
+                            //'file_ijazah',
+                            [
+                                'class' => ActionColumn::className(),
+                                'header' => 'Aksi',
+                                'template' => '{view}',
+                                'urlCreator' => function ($action, \backend\models\Pelamar $model, $key, $index, $column) {
+                                    return Url::toRoute([$action, 'nik' => $model->nik]);
+                                }
+                            ],
+                        ],
+                    ]); ?>
 
+                </div>
+            </div>
+        </div>
+    </div>
 
 </div>
--- backend/views/pelamar/view.php
+++ backend/views/pelamar/view.php
@@ -6,38 +6,55 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model backend\models\Pelamar */
 
-$this->title = $model->nik;
+$this->title = $model->nama_lengkap;
 $this->params['breadcrumbs'][] = ['label' => 'Pelamars', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 \yii\web\YiiAsset::register($this);
 ?>
 <div class="pelamar-view">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1>Nama : <?= Html::encode($this->title) ?></h1>
 
     <p>
-        <?= Html::a('Update', ['update', 'nik' => $model->nik], ['class' => 'btn btn-primary']) ?>
+        <!-- <?= Html::a('Update', ['update', 'nik' => $model->nik], ['class' => 'btn btn-primary']) ?>
         <?= Html::a('Delete', ['delete', 'nik' => $model->nik], [
             'class' => 'btn btn-danger',
             'data' => [
                 'confirm' => 'Are you sure you want to delete this item?',
                 'method' => 'post',
             ],
-        ]) ?>
+        ]) ?> -->
     </p>
 
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'nik',
+            [
+                'attribute' => 'nik',
+                'label' => 'NIK'
+            ],
             'nama_lengkap',
             'tempat_lahir',
             'tanggal_lahir',
             'alamat:ntext',
             'no_hp',
             'email:email',
-            'file_cv',
-            'file_ijazah',
+            [
+                'attribute' => 'file_cv',
+                'value' => function($model)
+                {
+                    return Html::a('<i class="fas fa-eye"></i> Lihat',['/pelamar/file','id' => $model->file_cv,'j' => "CV"],['target' => '_blank']);
+                },
+                'format' => 'raw'
+            ],
+            [
+                'attribute' => 'file_ijazah',
+                'value' => function($model)
+                {
+                    return Html::a('<i class="fas fa-eye"></i> Lihat',['/pelamar/file','id' => $model->file_ijazah,'j' => "Ijazah"],['target' => '_blank']);
+                },
+                'format' => 'raw'
+            ],
         ],
     ]) ?>
 
